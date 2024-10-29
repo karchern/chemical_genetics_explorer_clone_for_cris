444730a5a72c30357995f03e370dc9bc6ba00c27
Nicolai Karcher <karchernic@gmail.com>
2024-10-29 20:27:39 +0100

    force pull container in run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,4 @@
 # Set up common bash idiom
 set -euo pipefail
-docker run -p 3838:3838 registry.git.embl.de/grp-typas/chemical_genetics_explorer
+docker run --pull=always -p 3838:3838 registry.git.embl.de/grp-typas/chemical_genetics_explorer
 
